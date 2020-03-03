5fe441f54f08fa3cd4af959baf64faaaede877fd
Akihiko Tanabe <tanabe@determaind.biz>
2020-03-03 16:05:28 +0900

    propKeyを利用したmetadataの入出力に対応した

--- src/MetadataCollection.spec.ts
+++ src/MetadataCollection.spec.ts
@@ -10,16 +10,26 @@ type TestMetadata = {
 describe('MetadataCollection', function() {
   const testMetadataCollection = new MetadataCollection<TestMetadata>(TEST_KEY);
   const target = {};
+  const propKey = 'propKey';
 
   beforeEach(function() {
     Reflect.deleteMetadata(TEST_KEY, target);
+    Reflect.deleteMetadata(TEST_KEY, target, propKey);
   });
 
   it('set,get', function() {
     const testdata = { id: 'set', value: 'value1' };
+
     testMetadataCollection.set([testdata], target);
 
     expect(testMetadataCollection.get(target)).toEqual([testdata]);
+  });
+
+  it('set,get prop', function() {
+    const testdata = { id: 'set', value: 'prop' };
+    testMetadataCollection.set([testdata], target, propKey);
+
+    expect(testMetadataCollection.get(target, propKey)).toEqual([testdata]);
   });
 
   it('add', function() {
@@ -33,7 +43,29 @@ describe('MetadataCollection', function() {
       testMetadataCollection.add(testdata, target);
     });
 
+    expect(testMetadataCollection.get(target).length).toBe(testdataSet.length);
+
     testMetadataCollection.get(target).forEach((testdata, index) => {
+      expect(testdata).toBe(testdataSet[index]);
+    });
+  });
+
+  it('add prop', function() {
+    const testdataSet = [
+      { id: 'add', value: 'prop1' },
+      { id: 'add', value: 'prop2' },
+      { id: 'add', value: 'prop3' },
+    ];
+
+    testdataSet.forEach((testdata) => {
+      testMetadataCollection.add(testdata, target, propKey);
+    });
+
+    expect(testMetadataCollection.get(target, propKey).length).toBe(
+      testdataSet.length
+    );
+
+    testMetadataCollection.get(target, propKey).forEach((testdata, index) => {
       expect(testdata).toBe(testdataSet[index]);
     });
   });
@@ -45,5 +77,14 @@ describe('MetadataCollection', function() {
     testMetadataCollection.clear(target);
 
     expect(testMetadataCollection.get(target).length).toBe(0);
+  });
+
+  it('clear prop', function() {
+    const testdata = { id: 'clear', value: 'props' };
+    testMetadataCollection.add(testdata, target, propKey);
+
+    testMetadataCollection.clear(target, propKey);
+
+    expect(testMetadataCollection.get(target, propKey).length).toBe(0);
   });
 });
--- src/MetadataCollection.ts
+++ src/MetadataCollection.ts
@@ -9,21 +9,32 @@ export default class MetadataCollection<T> {
 
   KEY: symbol;
 
-  get(target: Target): T[] {
-    const value = Reflect.getMetadata(this.KEY, target);
+  get(target: Target, propKey?: string): T[] {
+    const value =
+      typeof propKey === 'string'
+        ? Reflect.getMetadata(this.KEY, target, propKey)
+        : Reflect.getMetadata(this.KEY, target);
 
     return Array.isArray(value) ? [...value] : [];
   }
 
-  set(value: T[], target: Target): void {
-    Reflect.defineMetadata(this.KEY, [...value], target);
+  set(value: T[], target: Target, propKey?: string): void {
+    if (typeof propKey === 'string') {
+      Reflect.defineMetadata(this.KEY, [...value], target, propKey);
+    } else {
+      Reflect.defineMetadata(this.KEY, [...value], target);
+    }
   }
 
-  add(value: T, target: Target): void {
-    this.set([...this.get(target), value], target);
+  add(value: T, target: Target, propKey?: string): void {
+    this.set([...this.get(target, propKey), value], target, propKey);
   }
 
-  clear(target: Target): void {
-    Reflect.deleteMetadata(this.KEY, target);
+  clear(target: Target, propKey?: string): void {
+    if (typeof propKey === 'string') {
+      Reflect.deleteMetadata(this.KEY, target, propKey);
+    } else {
+      Reflect.deleteMetadata(this.KEY, target);
+    }
   }
 }
