5034810276876794f3986e435dabfe2a4d2b5e07
Akihiko Tanabe <tanabe@determaind.biz>
2020-03-03 19:52:28 +0900

    exportするメソッドを加えた

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export * from './Controller';
+import Controller from './Controller';
+import Route from './Route';
+import { Get, Post, Put, Delete } from './Methods';
+import Middleware from './Middleware';
+
+export { Route, Controller, Get, Post, Put, Delete, Middleware };
